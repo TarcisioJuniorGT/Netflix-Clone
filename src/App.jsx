cfb256902263b28bd4b6212295f04b990ce64dab
Tarcísio Júnior <56036901+TarcisioJuniorGT@users.noreply.github.com>
2022-08-12 22:59:37 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Watch from './pages/Watch';
+import "./styles/app.scss";
+
+const App = () => {
+  return (
+      <Home />
+  )
+};
+
+export default App;
